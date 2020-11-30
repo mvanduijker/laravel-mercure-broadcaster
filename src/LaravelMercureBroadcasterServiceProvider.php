0801c26a2a01fef267f838e761d6e7594869c89b
Mark van Duijker <mark@duyker.nl>
2020-11-30 22:41:39 +0100

    :heavy_minus_sign: Remove support for lcobucci/jwt < 4.0.*

--- src/LaravelMercureBroadcasterServiceProvider.php
+++ src/LaravelMercureBroadcasterServiceProvider.php
@@ -5,9 +5,9 @@ namespace Duijker\LaravelMercureBroadcaster;
 use Duijker\LaravelMercureBroadcaster\Broadcasting\Broadcasters\MercureBroadcaster;
 use Illuminate\Broadcasting\BroadcastManager;
 use Illuminate\Support\ServiceProvider;
-use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\Configuration;
 use Lcobucci\JWT\Signer\Hmac\Sha256;
-use Lcobucci\JWT\Signer\Key;
+use Lcobucci\JWT\Signer\Key\InMemory;
 use Symfony\Component\Mercure\Publisher;
 
 class LaravelMercureBroadcasterServiceProvider extends ServiceProvider
@@ -21,11 +21,15 @@ class LaravelMercureBroadcasterServiceProvider extends ServiceProvider
                     new Publisher(
                         $config['url'],
                         function () use ($config) {
-                            $token = (new Builder())
-                                ->withClaim('mercure', ['publish' => ['*']])
-                                ->getToken(new Sha256(), new Key($config['secret']));
+                            $jwtConfiguration = Configuration::forSymmetricSigner(
+                                new Sha256(),
+                                InMemory::plainText($config['secret'])
+                            );
 
-                            return (string) $token;
+                            return $jwtConfiguration->builder()
+                                ->withClaim('mercure', ['publish' => ['*']])
+                                ->getToken($jwtConfiguration->signer(), $jwtConfiguration->signingKey())
+                                ->toString();
                         }
                     )
                 );
