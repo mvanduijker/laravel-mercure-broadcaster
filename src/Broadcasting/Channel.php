4744f19ff33334300b5b3579ebb2bf6a5dfc0679
Mark van Duijker <mark@duyker.nl>
2025-07-31 21:11:18 +0200

    chore: trigger deprecation on Channel class

--- src/Broadcasting/Channel.php
+++ src/Broadcasting/Channel.php
@@ -19,6 +19,8 @@ class Channel
 
     public function __construct(string $name, bool $private = false)
     {
+        trigger_deprecation('mvanduijker/laravel-mercure-broadcaster', '3,8.0', sprintf('Class %s is deprecated, use %s or for private channels use %s', __CLASS__, \Illuminate\Broadcasting\Channel::class, \Illuminate\Broadcasting\PrivateChannel::class));
+
         $this->name = $name;
         $this->private = $private;
     }
